1cd49dea4f98b91b6e510f6c70e02a73b1444549
PapaniaP11 <papaniap11@gmail.com>
2023-09-18 19:03:36 +0200

    Tests and changes 01

--- script.js
+++ script.js
@@ -26,3 +26,50 @@ function leaveGameScreen() {
 
 // Initial setup to show the arrow on the first menu item
 menuItems[0].classList.add("active");
+
+
+
+// Dialog Animation
+
+// JavaScript (script.js)
+
+const dialogText = [
+  "Hello, I'm Paolo Papania, a versatile creator in the fields of UX/UI design and Front-End development.",
+  "With a passion for crafting user-friendly experiences and a knack for turning ideas into pixel-perfect reality, I embark on a journey to bring imagination to life.",
+  
+];
+
+const dialogElement = document.getElementById('dialog-text');
+
+// Function to animate text typing effect for a single paragraph
+function typeTextForParagraph(text) {
+  return new Promise((resolve) => {
+      let index = 0;
+      const typingInterval = setInterval(() => {
+          if (index < text.length) {
+              dialogElement.innerHTML += text.charAt(index);
+              index++;
+          } else {
+              clearInterval(typingInterval);
+              setTimeout(() => {
+                  resolve();
+              }, 1000); // Adjust the delay between paragraphs (in milliseconds)
+          }
+      }, 30); // Adjust typing speed (smaller value for faster typing)
+  });
+}
+
+// Function to display all paragraphs with line breaks between them
+async function displayAllParagraphs(paragraphs) {
+  for (let i = 0; i < paragraphs.length; i++) {
+      if (i > 0) {
+          dialogElement.innerHTML += '<br><br>'; // Add line breaks between paragraphs
+      }
+      await typeTextForParagraph(paragraphs[i]); // Type the current paragraph
+  }
+}
+
+// Start displaying all paragraphs when the page loads
+window.onload = function () {
+  displayAllParagraphs(dialogText);
+};
