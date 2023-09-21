88b4e03c55f9d91c015d35308a5d4dc6ec4da293
PapaniaP11 <papaniap11@gmail.com>
2023-09-21 20:03:52 +0200

    Major change overall - New layout and css libraries -Nes.css

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-// Get all menu items
-const menuItems = document.querySelectorAll(".menu-item");
+// Function to open a profile in a new tab
+function openProfile(profileUrl) {
+	window.open(profileUrl, "_blank");
+}
 
-// Add event listeners to each menu item
-menuItems.forEach((menuItem) => {
-	menuItem.addEventListener("mouseenter", () => {
-		// Hide the arrow on all menu items
-		menuItems.forEach((item) => {
-			item.classList.remove("active");
-		});
-		// Show the arrow only on the hovered menu item
-		menuItem.classList.add("active");
-	});
-});
-
-// Function to handle leaving the game screen
-function leaveGameScreen() {
-	// Find the last hovered menu item and keep the arrow there
-	const lastHoveredMenuItem = document.querySelector(".menu-item.active");
-	if (lastHoveredMenuItem) {
-		lastHoveredMenuItem.classList.remove("active");
+// Function to open a profile based on the icon clicked
+function openProfileByIcon(icon) {
+	if (icon.classList.contains("github-icon")) {
+		const githubProfileUrl = "https://github.com/your-github-username";
+		openProfile(githubProfileUrl);
+	} else if (icon.classList.contains("linkedin-icon")) {
+		const linkedinProfileUrl =
+			"https://www.linkedin.com/in/your-linkedin-username/";
+		openProfile(linkedinProfileUrl);
 	}
 }
 
-// Initial setup to show the arrow on the first menu item
-menuItems[0].classList.add("active");
+// Get all elements with class 'profile-link' and add click event listeners
+const profileIcons = document.querySelectorAll(".profile-link");
+profileIcons.forEach((icon) => {
+	icon.addEventListener("click", () => openProfileByIcon(icon));
+});
